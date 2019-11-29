cc7b82216f4338b5d102741ba5ba2a7121f6f9a2
Wang Chenxu <git@wangchenxu.net>
2019-11-29 22:57:12 +0800

    chore: 搭建开发环境

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	plugins: [
+		'@babel/plugin-proposal-class-properties',
+		'@babel/plugin-proposal-do-expressions',
+		'@babel/plugin-proposal-function-bind',
+		'@babel/plugin-proposal-logical-assignment-operators',
+		'@babel/plugin-proposal-numeric-separator',
+		'@babel/plugin-proposal-object-rest-spread',
+		'@babel/plugin-proposal-optional-catch-binding',
+		'@babel/plugin-proposal-optional-chaining',
+		'@babel/plugin-proposal-private-methods',
+		'@babel/plugin-proposal-throw-expressions',
+		'@babel/plugin-transform-typescript',
+	],
+};
---
+++ rollup.config.js
@@ -0,0 +1,109 @@
+
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import dts from 'rollup-plugin-dts';
+import replace from 'rollup-plugin-replace';
+import { terser } from 'rollup-plugin-terser';
+import fsFn from 'fs';
+const { version } = JSON.parse(fsFn.readFileSync('./package.json'));
+
+const banner = `
+/*!
+ * vue-router v${ version }
+ * (c) ${ new Date().getFullYear() } Wang Chenxu
+ * @license MIT
+ */
+`;
+export default [
+	{
+		input: 'src/index.ts',
+		output: [
+			{
+				file: 'dist/vue-insertable.common.js',
+				sourcemap: true,
+				format: 'cjs',
+				banner,
+				exports: 'default',
+			},
+			{
+				file: 'dist/vue-insertable.esm.js',
+				sourcemap: true,
+				format: 'esm',
+				banner,
+			},
+		],
+		plugins: [
+			resolve({ extensions: [ '.mjs', '.js', '.jsx', '.json', '.ts', '.tsx' ] }),
+			babel({ extensions: [ '.mjs', '.js', '.jsx', '.es', '.ts', '.tsx' ] }),
+			replace({ __VERSION__: version }),
+		],
+	},
+	{
+		input: 'src/browser.ts',
+		output: [
+			{
+				file: 'dist/vue-insertable.js',
+				sourcemap: true,
+				format: 'umd',
+				name: 'VueInsertable',
+				banner,
+				exports: 'default',
+				globals: {
+					vue: 'Vue',
+				},
+			},
+		],
+		plugins: [
+			resolve({ extensions: [ '.mjs', '.js', '.jsx', '.json', '.ts', '.tsx' ] }),
+			babel({ extensions: [ '.mjs', '.js', '.jsx', '.es', '.ts', '.tsx' ] }),
+			replace({ __VERSION__: version }),
+		],
+		external: ['vue'],
+	},
+	{
+		input: 'src/browser.ts',
+		output: [
+			{
+				file: 'dist/vue-insertable.min.js',
+				sourcemap: true,
+				format: 'umd',
+				name: 'VueInsertable',
+				exports: 'default',
+				banner,
+				globals: {
+					vue: 'Vue',
+				},
+			},
+		],
+		plugins: [
+			resolve({ extensions: [ '.mjs', '.js', '.jsx', '.json', '.ts', '.tsx' ] }),
+			babel({ extensions: [ '.mjs', '.js', '.jsx', '.es', '.ts', '.tsx' ] }),
+			replace({ __VERSION__: version }),
+			terser(),
+		],
+		external: ['vue'],
+	},
+	{
+		input: 'src/index.ts',
+		output: [
+			{
+				file: 'dist/vue-insertable.esm.min.js',
+				sourcemap: true,
+				format: 'esm',
+				banner,
+			},
+		],
+		plugins: [
+			resolve({ extensions: [ '.mjs', '.js', '.jsx', '.json', '.ts', '.tsx' ] }),
+			babel({ extensions: [ '.mjs', '.js', '.jsx', '.es', '.ts', '.tsx' ] }),
+			replace({ __VERSION__: version }),
+			terser(),
+		],
+	},
+	{
+		input: [ 'src/index.ts' ],
+		output: [ { file: 'types.d.ts', format: 'esm', banner } ],
+		plugins: [ dts() ],
+		external: ['vue'],
+	},
+];
